76476a91eb0a0d36466f67434590ce3670bca026
Carlos0354 <ce.ramirez.r@edu.utc.mx>
2021-11-22 17:00:13 -0600

    figuras

---
+++ 011_figuras/principal.java
@@ -0,0 +1,15 @@
+
+
+/*
+Ejemplo de una clase de figuras
+*/
+
+public class principal{
+
+    public static void main(String[] args){
+        //metodo principal
+        //instancia de la clase
+        figura objfigura = new figura();
+        objfigura.menu();
+    }
+}
